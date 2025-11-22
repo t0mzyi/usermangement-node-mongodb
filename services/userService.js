474cc479a9f09cf8ac2d9c12e5c554aba8d1b20e
T0mzyy <jaideepkollys@gmail.com>
2025-11-22 23:52:15 +0530

    login n signup donee

--- services/userService.js
+++ services/userService.js
@@ -10,34 +10,8 @@ export const registerUser = async (UserData) => {
     }
     const existingUser = await UserDb.findOne({ email: email });
 
-<<<<<<< HEAD
     if (existingUser) {
       return { status: false, message: "Email already exists" };
-=======
-    try {
-        const {userName,email,password} = UserData
-        const existinguserName =  await UserDb.findOne({userName : userName})
-        if(existinguserName){
-            return{status :false, message : "userName already exists please choose another name"}
-        }
-        const  existingUser = await UserDb.findOne({email : email})
-
-        if(existingUser){
-            return{status : false, message: "Email already exists"}
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new UserDb({
-            userName : userName,
-            email :email,
-            password : hashedPassword
-        })
-        await newUser.save()
-        return {status : true ,message : "User created sucessfull please login"}
-
-    }catch(err){
-        console.log(`err in registerUser`,err)
->>>>>>> 88897df4d4bc10bbacb507672a660117ff18c639
     }
 
     const hashedPassword = await bcrypt.hash(userpass, 10);
@@ -54,7 +28,6 @@ export const registerUser = async (UserData) => {
 };
 
 export const loginUser = async (loginData) => {
-<<<<<<< HEAD
   try {
     const { email, userpass } = loginData;
 
@@ -62,26 +35,6 @@ export const loginUser = async (loginData) => {
     if (!user) {
       console.log(`Someone tried to enter`, user);
       return { status: false, message: "Email not found" };
-=======
-    try{
-        const {email,password} = loginData;
-
-        const user = await UserDb.findOne({email : email})
-        if(!user){
-            console.log(`Someone tried to enter`,user)
-            return {status: false, message : "Email not found"}
-        }
-
-        const matchPass = await bcrypt.compare(password, user.password)
-        if(!matchPass){
-            console.log(`someone entered wrong password`,matchPass)
-            return {status :  false, message : "Incorrect Passwordd"}
-        }
-        console.log(`login sucessfull`,user)
-        return {status:true, message: "Login Sucessful", user}
-    }catch(err){
-        console.log(`err in loginuser service`,err)
->>>>>>> 88897df4d4bc10bbacb507672a660117ff18c639
     }
 
     const matchPass = await bcrypt.compare(userpass, user.password);
