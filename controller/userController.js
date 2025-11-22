953d899d53a001a42f3c35fbfdaa6cfec5ddc5a8
T0mzyy <jaideepkollys@gmail.com>
2025-11-22 23:48:51 +0530

    login n signup done

--- controller/userController.js
+++ controller/userController.js
@@ -1,112 +1,106 @@
-import {registerUser,loginUser} from '../services/userService.js'
-import { validationResult } from 'express-validator'
+import { registerUser, loginUser } from "../services/userService.js";
+import { validationResult } from "express-validator";
 
 const loginGet = (req, res) => {
-    res.render('user/login', { 
-        success: req.session.success || null,
-        errors: req.session.errors || {},   
-        old: req.session.old || {},         
-        message: req.session.error || null
-    });
+  res.render("user/login", {
+    success: req.session.success || null,
+    errors: req.session.errors || {},
+    old: req.session.old || {},
+    message: req.session.error || null,
+  });
 
-    req.session.success = null;
-    req.session.errors = null;
-    req.session.old = null;
-    req.session.error = null;
+  req.session.success = null;
+  req.session.errors = null;
+  req.session.old = null;
+  req.session.error = null;
 };
 
-const registerGet = (req,res) => {
-    res.render('user/register' ,{
-        errors: req.session.errors || {},
-        old : req.session.old || {},
-        message : req.session.error
-    })
-    req.session.errors = null
-    req.session.old = null
+const registerGet = (req, res) => {
+  res.render("user/register", {
+    errors: req.session.errors || {},
+    old: req.session.old || {},
+    message: req.session.error,
+  });
+  req.session.errors = null;
+  req.session.old = null;
+};
 
-}
+const registerPost = async (req, res) => {
+  try {
+    req.session.error = null;
 
-const registerPost = async (req,res) => {
-    try{
-        req.session.error = null
+    const errors = validationResult(req);
 
-        const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      const formattedErrors = {};
+      errors.array().forEach((err) => (formattedErrors[err.path] = err.msg));
 
-        if (!errors.isEmpty()) {
-           const formattedErrors = {}
-           errors.array().forEach(err => formattedErrors[err.param] = err.msg)
+      req.session.errors = formattedErrors;
+      req.session.old = req.body;
 
-           req.session.errors = formattedErrors;
-           req.session.old = req.body
-        
-           return res.redirect('/registerPage')
-         
-
-        }
-
-        const result = await registerUser(req.body)
-
-        if(!result.status){
-            req.session.error = result.message
-            return res.redirect('/registerPage')
-        }
-        req.session.success = result.message
-        return res.redirect('/loginPage')
-        
-    }catch(err){
-        console.log(`err in register post`,err)
+      return res.redirect("/registerPage");
     }
-} 
 
-const loginPost = async (req,res) => {
-    try{
-        req.session.error = null
-        const errors = validationResult(req)
-        if(!errors.isEmpty()){
-            const formattedErrors = {}
-            errors.array().forEach(err => formattedErrors[err.param] = err.msg)
+    const result = await registerUser(req.body);
 
-            req.session.errors = formattedErrors;
-            req.session.old =  req.body
-
-            return res.redirect('/loginPage')
-        }
-
-        const result = await loginUser(req.body)
-
-        if(!result.status) {
-            req.session.error = result.message;
-            req.session.old = req.body;
-            return res.redirect('/loginPage')
-        }
-
-        req.session.user = {
-            id: result.user._id,
-            name : result.user.userName,
-            email : result.user.email
-        }
-        req.session.success = result.message;
-        return res.redirect('/home')
-    }catch(err) {
-        console.log(`err in loginPost`,err)
-        return res.status(500).send("server error")
+    if (!result.status) {
+      req.session.error = result.message;
+      return res.redirect("/registerPage");
     }
-}
+    req.session.success = result.message;
+    return res.redirect("/loginPage");
+  } catch (err) {
+    console.log(`err in register post`, err);
+  }
+};
+
+const loginPost = async (req, res) => {
+  try {
+    req.session.error = null;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const formattedErrors = {};
+      errors.array().forEach((err) => (formattedErrors[err.path] = err.msg));
+
+      req.session.errors = formattedErrors;
+      req.session.old = req.body;
+
+      return res.redirect("/loginPage");
+    }
+
+    const result = await loginUser(req.body);
+
+    if (!result.status) {
+      req.session.error = result.message;
+      req.session.old = req.body;
+      return res.redirect("/loginPage");
+    }
+
+    req.session.user = {
+      id: result.user._id,
+      name: result.user.userName,
+      email: result.user.email,
+    };
+    req.session.success = result.message;
+    return res.redirect("/home");
+  } catch (err) {
+    console.log(`err in loginPost`, err);
+    return res.status(500).send("server error");
+  }
+};
 
 export const logout = async (req, res) => {
-    try {
-        req.session.destroy(err => {
-            if(err) {
-                return res.send("Error Logging out")
-            }
-            res.clearCookie("connect.sid")
-            res.redirect('/')
-        })
-    } catch (error) {
-        console.log(`error from logot user ${error}`);
-    }
-}
+  try {
+    req.session.destroy((err) => {
+      if (err) {
+        return res.send("Error Logging out");
+      }
+      res.clearCookie("connect.sid");
+      res.redirect("/");
+    });
+  } catch (error) {
+    console.log(`error from logot user ${error}`);
+  }
+};
 
-
-
-export default {loginGet,registerGet,registerPost,loginPost,logout}
+export default { loginGet, registerGet, registerPost, loginPost, logout };
--- services/userService.js
+++ services/userService.js
@@ -1,56 +1,50 @@
-import UserDb from '../models/dbSchema.js'
-import bcrypt from 'bcrypt';
-
-
+import UserDb from "../models/dbSchema.js";
+import bcrypt from "bcrypt";
 
 export const registerUser = async (UserData) => {
+  try {
+    const { userName, email, userpass } = UserData;
+    const existinguserName = await UserDb.findOne({ userName: userName });
+    if (existinguserName) {
+      return {status: false,message: "userName already exists please choose another name",};
+    }
+    const existingUser = await UserDb.findOne({ email: email });
 
-    try {
-        const {userName,email,userpass} = UserData
-        const existinguserName =  await UserDb.findOne({userName : userName})
-        if(existinguserName){
-            return{status :false, message : "userName already exists please choose another name"}
-        }
-        const  existingUser = await UserDb.findOne({email : email})
-
-        if(existingUser){
-            return{status : false, message: "Email already exists"}
-        }
-
-        const hashedPassword = await bcrypt.hash(userpass, 10);
-        const newUser = new UserDb({
-            userName : userName,
-            email :email,
-            password : hashedPassword
-        })
-        await newUser.save()
-        return {status : true ,message : "User created sucessfull please login"}
-
-    }catch(err){
-        console.log(`err in registerUser`,err)
+    if (existingUser) {
+      return { status: false, message: "Email already exists" };
     }
 
-}
+    const hashedPassword = await bcrypt.hash(userpass, 10);
+    const newUser = new UserDb({
+      userName: userName,
+      email: email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    return { status: true, message: "User created sucessfull please login" };
+  } catch (err) {
+    console.log(`err in registerUser`, err);
+  }
+};
 
 export const loginUser = async (loginData) => {
-    try{
-        const {email,userpass} = loginData;
+  try {
+    const { email, userpass } = loginData;
 
-        const user = await UserDb.findOne({email : email})
-        if(!user){
-            console.log(`Someone tried to enter`,user)
-            return {status: false, message : "Email not found"}
-        }
-
-        const matchPass = await bcrypt.compare(userpass, user.password)
-        if(!matchPass){
-            console.log(`someone entered wrong password`,matchPass)
-            return {status :  false, message : "Incorrect Passwordd"}
-        }
-        console.log(`login sucessfull`,user)
-        return {status:true, message: "Login Sucessful", user}
-    }catch(err){
-        console.log(`err in loginuser service`,err)
+    const user = await UserDb.findOne({ email: email });
+    if (!user) {
+      console.log(`Someone tried to enter`, user);
+      return { status: false, message: "Email not found" };
     }
-}
 
+    const matchPass = await bcrypt.compare(userpass, user.password);
+    if (!matchPass) {
+      console.log(`someone entered wrong password`, matchPass);
+      return { status: false, message: "Incorrect Passwordd" };
+    }
+    console.log(`login sucessfull`, user);
+    return { status: true, message: "Login Sucessful", user };
+  } catch (err) {
+    console.log(`err in loginuser service`, err);
+  }
+};
